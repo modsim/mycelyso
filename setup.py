95a61550ad24a97f73e8b77ebfe776a1ecef15f4
Christian Sachs <c.sachs@fz-juelich.de>
2017-05-13 13:58:25 +0200

    cleanup of setup.py clarifying target Python version

--- setup.py
+++ setup.py
@@ -9,25 +9,18 @@ from setuptools import setup, find_packages
 setup(
     name='mycelyso',
     version='0.0.1',
-    description='MYCEL anaLYsis SOftware',
-    long_description='',
+    description='MYCEl anaLYsis SOftware',
+    long_description='see https://github.com/modsim/mycelyso',
     author='Christian C. Sachs',
     author_email='c.sachs@fz-juelich.de',
     url='https://github.com/modsim/mycelyso',
     packages=find_packages(),
-    # scripts=[''],
     install_requires=[
         'numpy', 'scipy', 'scikit-image>=0.12', 'networkx', 'tables', 'numexpr', 'pandas',
         'tifffile', 'nd2file',
         'mfisp_boxdetection', 'molyso',
         'tqdm'  # nicer progress bars, using the MPLv2+MIT licensed version
     ],
-    extras_require={
-        'fast_segmentation': ['numexpr'],
-    },
-    # package_data={
-    #     'package': ['additional/file.dat'],
-    # },
     license='BSD',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -36,11 +29,10 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: POSIX',
         'Operating System :: POSIX :: Linux',
-        'Operating System :: MacOS :: MacOS X',   # lately no tests
+        'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python :: 2.7',  # tests, not often
-        'Programming Language :: Python :: 3',    #
-        'Programming Language :: Python :: 3.4',  # main focus
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: Scientific/Engineering :: Image Recognition',
     ]
